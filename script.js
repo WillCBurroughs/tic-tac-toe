dfe796e5025a94d1887c495c4d5f16d6b39b8ea8
William Burroughs <williamburroughs@Williams-MBP.localdomain>
2023-10-05 11:43:13 -0400

    adding functionality of values to array

--- script.js
+++ script.js
@@ -2,9 +2,15 @@
 let gameOver = false; 
 let playerOneTurn = true; 
 
+// Will be changed and evaluate for win conditions
+let storeConditions = [["","",""],["","",""],["","",""]];
+
 let columnTic = makeCol();
+addMarkforArray(columnTic,0,0);
 let secondTic = makeCol(); 
+addMarkforArray(secondTic,0,1);
 let thirdTic = makeCol(); 
+addMarkforArray(thirdTic,0,2);
 
 let containerTic = document.createElement("div");
 containerTic.classList.add("container-fluid"); 
@@ -20,8 +26,11 @@ holdRow.appendChild(thirdTic);
 containerTic.appendChild(holdRow);
 
 let fourthTic = makeCol();
+addMarkforArray(fourthTic, 1, 0);
 let fifthTic = makeCol(); 
+addMarkforArray(fifthTic, 1, 1);
 let sixthTic = makeCol(); 
+addMarkforArray(sixthTic, 1, 2);
 
 let holdSecondRow = makeRow(); 
 
@@ -29,8 +38,11 @@ holdSecondRow.append(fourthTic,fifthTic,sixthTic);
 containerTic.appendChild(holdSecondRow);
 
 let seventhTic = makeCol(); 
+addMarkforArray(seventhTic, 2, 0);
 let eigthTic = makeCol(); 
+addMarkforArray(eigthTic, 2, 1);
 let ninthTic = makeCol(); 
+addMarkforArray(ninthTic, 2, 2);
 
 let bottomRow = makeRow(); 
 bottomRow.append(seventhTic, eigthTic, ninthTic);
@@ -86,6 +98,66 @@ function makeCol(){
 
     return tempCol;
 }
+
+// Only allow for this function to be called if column 
+// Not selected, can remove if selected 
+function addMarkforArray(object, row, col){
+
+    // Can check and execute this only if object 
+    // Contains class selected 
+
+    if(!object.classList.contains("selected")){
+        object.addEventListener("click", function(){
+        if(playerOneTurn){
+            storeConditions[row][col] = "x";
+        } else {
+            storeConditions[row][col] = "o";
+        }
+
+        console.log(storeConditions);
+    })
+
+    }
+
+    
+
+    console.log(storeConditions);
+
+}
+
+// Can have multiple. Test for horizontal win first 
+// All win conditions will evaluate true or false 
+// Call others after 
+
+function testWin(){
+
+}
+ 
+
+function horizontalWin(){
+    
+}
+
+function verticalWin(){
+
+}
+
+function diagonalWin(){
+
+}
+
+// Test if no value in the array evaluates to falsy
+function testDraw(){
+
+}
+
+// Needs to set array to empty 
+// Needs to set gameOver to false 
+
+function resetGame(){
+
+}
+
 
 function makeRow(){
     let tempRow = document.createElement("div");
